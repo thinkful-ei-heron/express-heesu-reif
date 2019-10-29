f8b376e599ee96fe68ebf8f9387a36d0281d0213
Heesu Kang <hskang135@gmail.com>
2019-10-29 11:48:02 -0400

    doing

--- app.js
+++ app.js
@@ -21,21 +21,25 @@ app.get('/apps', (req, res) => {
   if ('sort' in req.query) {
     if (req.query.sort == 'app') {
       // sort by App (app name)
-      filteredData = appsData.sort((a, b) => (a.App > b.App ? 1 : -1));
+      sortedData = appsData.sort((a, b) => (a.App > b.App ? 1 : -1));
     } else if (req.query.sort == 'rating') {
       // sort by rating
-      filteredData = appsData.sort((a, b) => (a.Rating > b.Rating ? 1 : -1));
+      sortedData = appsData.sort((a, b) => (a.Rating > b.Rating ? 1 : -1));
     }
   } else {
     // default list
-    filteredData = appsData;
+    sortedData = appsData;
   }
 
   if ('genre' in req.query) {
+    if(['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].indexOf(req.query.genre) >= 0) {
+      sortedData=sortedData.filter((item) => item.Genres.toLowerCase().includes(req.query.genre))
+    } else {
+      res.status(400).json({error: 'genre does not exist'});
   }
 
-  res.status(200).json(returnData);
-});
+  res.send(sortedData);
+}});
 
 app.listen(9001, () => {
   console.log('listening to port 9001');
